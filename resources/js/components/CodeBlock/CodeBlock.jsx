6c9a093529bf0002bdd2c6071558d40b3299692f
hasanmoriya <developer.hasan429@gmail.com>
2024-12-17 12:30:34 +0530

    continue development...

--- resources/js/components/CodeBlock/CodeBlock.jsx
+++ resources/js/components/CodeBlock/CodeBlock.jsx
@@ -1,20 +1,27 @@
-// src/components/CodeBlock/CodeBlock.jsx
-import React from 'react';
+import React, { useRef } from 'react';
+import { Toast } from 'primereact/toast';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 const CodeBlock = ({ title, description, buttonText, copyContent }) => {
+
+  const toast = useRef(null);
+
+  const showSuccess = (message) => {
+    toast.current.show({ label: 'Success', severity: 'success', summary: 'Success', detail: message, life: 3000 });
+  };
+
   const handleCopy = () => {
-    navigator.clipboard
-      .writeText(copyContent)
-      .then(() => alert('Copied to clipboard!'))
-      .catch((error) => console.error('Failed to copy text:', error));
+    navigator.clipboard.writeText(copyContent);
+    showSuccess('Copied to clipboard!');
   };
 
   return (
     <div className="bg-theme-dark text-gray-100 rounded-lg shadow-lg overflow-hidden">
+      <Toast ref={toast} />
       <div className="flex justify-between items-center px-4 py-3 bg-white border-b border-theme-dark">
         <span className="text-sm font-semibold text-gray-900">{title}</span>
-        <button
-          onClick={handleCopy}
+        
+        <button onClick={handleCopy}
           className="text-sm bg-gray-800 hover:bg-gray-700 text-white px-3 py-1 rounded focus:outline-none focus:ring-2 focus:ring-theme-accent"
         >
           {buttonText}
