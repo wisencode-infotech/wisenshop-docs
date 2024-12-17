96d4b5a23d74e561ef27dc25f8745bf6ac39bfb4
Mahammadali <manknojiya121@gmail.com>
2024-12-17 11:29:53 +0530

    Improvements

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\General;
 use App\Http\Controllers\Controller;
 use App\Models\Topic;
-use App\Models\BlockType;
 use App\Models\TopicBlock;
 use Illuminate\Http\Request;
 
@@ -19,16 +19,33 @@ class SearchController extends Controller
             return response()->json([]);
         }
 
-        // Search in topics
-        $topics = Topic::where('name', 'like', "%{$query}%")
-            ->select('id', 'name', 'slug')
-            ->limit(5)
-            ->get();
+        // Get the cached topics (if available) or load them from the database and cache them
+        $topics = General::cacheForever('topics', function() {
+            return Topic::all(); // Cache all topics forever if not cached
+        });
 
-        $topic_blocks = TopicBlock::select('id', 'attributes', 'topic_id')
-            ->with('topic:id,name,slug')
-            ->get()
-            ->filter(function($topic_block) use ($query) {
+        // Filter the cached topics using collection's `filter` method
+        $searched_topics = $topics->filter(function($topic) use ($query) {
+            return stripos($topic->name, $query) !== false; // Case-insensitive search
+        });
+
+        // Limit the result to 5 items and map the necessary fields
+        $topics = $searched_topics->take(5)->values()->map(function($topic) {
+            return [
+                'id' => $topic->id,
+                'name' => $topic->name,
+                'slug' => $topic->slug,
+            ];
+        });
+
+        // Get the cached topic blocks (if available) or load them from the database and cache them
+        $all_topic_blocks = General::cacheForever('topic_blocks', function() {
+            return TopicBlock::select('id', 'attributes', 'topic_id')
+                ->with('topic:id,name,slug')
+                ->get(); // Cache all topic blocks forever if not cached
+        });
+
+        $topic_blocks = $all_topic_blocks->filter(function($topic_block) use ($query) {
                 $attributes = json_decode($topic_block->attributes, true);
 
                 foreach ($attributes as $key => $value) {
