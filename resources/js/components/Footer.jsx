ef3e22010e8960cca5b0818ad7accf6e568416a6
Mahammadali <manknojiya121@gmail.com>
2024-12-18 18:21:03 +0530

    Improvements

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <footer className="bg-theme-dark text-center py-2">
-            <p className="text-sm text-white">&copy; 2024 WisenShop. All Rights Reserved.</p>
+            <p className="text-sm text-white"><i className="fa-solid fa-code"></i> Built by WisenDocs.</p>
         </footer>
     );
 };
