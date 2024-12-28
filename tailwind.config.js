212c39812f8018a378fde068406fbd29c463bc2e
Mahammadali <manknojiya121@gmail.com>
2024-12-28 15:16:53 +0530

    Improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,16 +16,16 @@ module.exports = {
             colors: {
                 theme: {
                     DEFAULT: '#3d5f8a', // Default primary color (light mode)
-                    dark: '#1e2a47', // Dark mode primary color (darker shade of blue)
-                    light: '#CAF0F8', // Light theme accent color (soft light blue)
+                    dark: '#0E0D0E', // Dark mode primary color (darker shade of blue)
+                    light: '#F5FDFD', // Light theme accent color (soft light blue)
                     accent: '#F1F8FF', // Light theme accent background (pale light blue)
                     code: '#4de77a', // Code highlighting color (green)
                     codebg: '#3f3f3f', // Code background (dark gray)
-                    lightBackground: '#ffffff', // Light mode background color (white)
+                    lightBackground: '#dededed', // Light mode background color (white)
                     lightText: '#333333', // Light mode text color (dark gray)
                     lightBorder: '#e0e0e0', // Light mode border color (light gray)
                     darkBackground: '#121212', // Dark mode background color (blackish)
-                    darkText: '#e0e0e0', // Dark mode text color (light gray)
+                    darkText: '#ffffff', // Dark mode text color (light gray)
                     darkBorder: '#444444', // Dark mode border color (darker gray)
                 },
             },
