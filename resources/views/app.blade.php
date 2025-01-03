ff71f93266beb64db43713919067a86a05f2ae79
Mahammadali <manknojiya121@gmail.com>
2025-01-03 15:39:23 +0530

    Improvements

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <title>WisenShop Documentation</title>
+    <title>WisenShop Documentation - Multipurpose eCommerce script</title>
+    <link rel="icon" href="{{ asset('assets/images/favicon.png') }}" type="image/png">
     @viteReactRefresh
     @vite(['resources/js/app.jsx'])
 </head>
